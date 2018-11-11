6ad69fa623fe2d86f779bc43f5ead4c207172576
Tanmay Patil <tanmayrmfc@gmail.com>
2018-11-11 16:19:16 +0530

    changed browserrouter to hasrouter for gh deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
@@ -14,27 +14,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Provider>
-        <Router>
-          <div className="App">
-            <Header branding="Contact Manager" />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Contacts} />
-                <Route exact path="/contact/add" component={AddContact} />
-                <Route exact path="/contact/edit/:id" component={EditContact} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/test" component={Test} />
-                <Route component={NotFound} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider>
+                <Router>
+                    <div className="App">
+                        <Header branding="Contact Manager" />
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Contacts} />
+                                <Route
+                                    exact
+                                    path="/contact/add"
+                                    component={AddContact}
+                                />
+                                <Route
+                                    exact
+                                    path="/contact/edit/:id"
+                                    component={EditContact}
+                                />
+                                <Route exact path="/about" component={About} />
+                                <Route exact path="/test" component={Test} />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 export default () => {
-  return (
-    <div>
-      <h1 className="display-4">About Contact Manager</h1>
-      <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">Version 1.0.0</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="display-4">About Contact Manager</h1>
+            <p className="lead">
+                Simple app created by Tanmay Patil to manage contacts
+            </p>
+            <p className="text-secondary">Version 1.0.0</p>
+            <p className="text-secondary">
+                - This is a React JS based application designed using Bootstrap
+                4.
+            </p>
+            <p className="text-secondary">
+                - This application makes use of JSON placeholder API to
+                playaround with temporary data and perform requests.
+            </p>
+            <p className="text-secondary">
+                - This application makes use of Context API which is a simple
+                alternative to Redux
+            </p>
+            <p className="text-secondary">
+                - I have another Contact Manager application which is created
+                using Redux instead Context API so don't forget to check my
+                github account
+            </p>
+        </div>
+    );
 };
